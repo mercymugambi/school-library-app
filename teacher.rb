58bda8f392310e80812af875de83eb6027076918
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-06-21 12:00:09 +0300

    Update teacher.rb file

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(id, age, specialization, parent_permission: true, name: 'Unknown')
+    super(id, age, parent_permission: parent_permission, name: name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
