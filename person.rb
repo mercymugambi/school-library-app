960940c747afec76798d5835d1f9aeaef98f7ed2
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-06-21 11:29:10 +0300

    Add getters and setters

--- person.rb
+++ person.rb
@@ -1,7 +1,27 @@
 Class Person
+attr_reader :id
+
 def
-initialize(id, name, age)
+initialize(id, name = "Unknown", age, parent_permission = true)
   @id = id
   @name = name
   @age = age
+  @parent_permission = parent_permission
+end
+
+def name
+    @name
+end
+def age
+    @age
+end
+
+def name (new_name)
+    @name = new_name
+end
+
+def age (new_age)
+    @age = new_age
+end
+
 end
