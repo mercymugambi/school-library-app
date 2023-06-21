5da32506ae42258b70feb1254a4889bedda4de62
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-06-21 11:45:24 +0300

    Update class person

--- person.rb
+++ person.rb
@@ -1,27 +1,21 @@
-Class Person
-attr_reader :id
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
 
-def
-initialize(id, name = "Unknown", age, parent_permission = true)
-  @id = id
-  @name = name
-  @age = age
-  @parent_permission = parent_permission
-end
+  def initialize(id, age, parent_permission: true, name: 'Unknown')
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-def name
-    @name
-end
-def age
-    @age
-end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-def name (new_name)
-    @name = new_name
-end
+  private
 
-def age (new_age)
-    @age = new_age
-end
-
+  def of_age?
+    @age >= 18
+  end
 end
